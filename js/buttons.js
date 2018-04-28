8c658135d84b6cbcbc2f246377b37988f60d4044
rigwars <janixwow@gmail.com>
2018-04-28 15:36:14 +0200

    Fund ICO button allow 5% desync between Front end and Back end
    Fund ICO button allow 5% desync between Front end and Back end

--- js/buttons.js
+++ js/buttons.js
@@ -23,7 +23,7 @@ $(function(){
 $(function(){
     $(".ico-buy-button").click(function(){
        var slider_val = $("#myRange").val();
-         var invest_amount = (slider_val*game.futurebalance)/100;
+         var invest_amount = (slider_val*game.futurebalance)/105; // 5% can be desinced worst case!
          FundICO(invest_amount);
     });
 });
