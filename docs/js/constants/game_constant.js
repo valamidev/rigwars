ff469658326e7e8d21cb652d84188153bae279e6
T <52665225+valamidev@users.noreply.github.com>
2023-08-05 11:31:01 +0200

    CleanUp

--- docs/js/constants/game_constant.js
+++ docs/js/constants/game_constant.js
@@ -1,25 +1,26 @@
-
 gametext = [];
 
 gametext.error = [];
 
-gametext.error[0] = "<p>Please install or activate your Metamask Plugin in your browser. <br> You can't play RigWars without Metamask installed.</p>";
-gametext.error[1] = "<p>Your Metamask locked or you have no valid address please reload the page!</p>";
+gametext.error[0] =
+  "<p>Please install or activate your Metamask Plugin in your browser. <br> You can't play RigWars without Metamask installed.</p>";
+gametext.error[1] =
+  "<p>Your Metamask locked or you have no valid address please reload the page!</p>";
 gametext.error[2] = `<p>Please allow your Metamask to connect to this website, and make sure that you are using the following network:<br>
 <center>
-Network Name: cheapETH <br>
-New RPC URL: https://node.cheapeth.org/rpc <br>
-Chain ID: 777 <br>
-Currency Symbol: cTH <br>
+Network Name: rEthereum <br>
+New RPC URL: https://rpc.rethereum.org/ <br>
+Chain ID: 622277 <br>
+Currency Symbol: RTH <br>
+Block explorer URL: https://explorer.rethereum.org/ <br>
 </center>
 </p>`;
-gametext.welcome = 
-"<span>C:/Mining/Start > RigWarsBeta0.1.exe --tutorial</span> <br> Welcome on RigWars.io the Original Crypto Mining Idle game DAPP <br><br>"+
-"- Setup your own Mining Rig and help the Futurehash network to mint more and more Coin. "+
-"Being a miner is a dangerous profession recruit your own Cyber Army to protect your Coins or attack others and steal theirs.<br>"+
-"Invest your Coins to ICO everyday and get back up to 20% dividens from Network HODL!<br><br><br><br>"+
-"Press Start Game Button on the right side to continue...";
-
+gametext.welcome =
+  "<span>C:/Mining/Start > RigWarsBeta0.1.exe --tutorial</span> <br> Welcome on RigWars.io the Original Crypto Mining Idle game DAPP <br><br>" +
+  "- Setup your own Mining Rig and help the Futurehash network to mint more and more Coin. " +
+  "Being a miner is a dangerous profession recruit your own Cyber Army to protect your Coins or attack others and steal theirs.<br>" +
+  "Invest your Coins to ICO everyday and get back up to 20% dividens from Network HODL!<br><br><br><br>" +
+  "Press Start Game Button on the right side to continue...";
 
 //                   price,           prod.     upgrade,        priceETH, limit
 /*
@@ -36,16 +37,51 @@ rigData[8] = RigData(100000000000000, 30000000, 50000000000000,  0,          256
 
 rigData = [];
 
-rigData[0] = {price: 128,    eth:  0,     prod: 1,       upgrade: 64,   limit: 64};
-rigData[1] = {price: 1024,     eth:  0,      prod: 64,       upgrade: 512,   limit: 64};
-rigData[2] = {price: 204800,    eth:  0,       prod: 1024,       upgrade: 102400,   limit: 128};
-rigData[3] = {price: 25600000,    eth:  0,       prod: 8192,       upgrade: 12800000,   limit: 128};
-rigData[4] = {price: 0,   eth:  0.5,        prod: 65536,       upgrade: 30000000000,   limit: 256};
-rigData[5] = {price: 30000000000,  eth:  0,         prod: 100000,       upgrade: 10000000000,   limit: 256};
-rigData[6] = {price: 300000000000,   eth:  0,        prod: 500000,       upgrade: 100000000000,   limit: 256};
-rigData[7] = {price: 0,   eth:  5.0,        prod: 3000000,       upgrade: 12500000000000,   limit: 256};
-rigData[8] = {price: 100000000000000,  eth:  0,         prod: 30000000,       upgrade: 50000000000000,   limit: 256};
-
+rigData[0] = { price: 128, eth: 0, prod: 1, upgrade: 64, limit: 64 };
+rigData[1] = { price: 1024, eth: 0, prod: 64, upgrade: 512, limit: 64 };
+rigData[2] = { price: 204800, eth: 0, prod: 1024, upgrade: 102400, limit: 128 };
+rigData[3] = {
+  price: 25600000,
+  eth: 0,
+  prod: 8192,
+  upgrade: 12800000,
+  limit: 128,
+};
+rigData[4] = {
+  price: 0,
+  eth: 0.5,
+  prod: 65536,
+  upgrade: 30000000000,
+  limit: 256,
+};
+rigData[5] = {
+  price: 30000000000,
+  eth: 0,
+  prod: 100000,
+  upgrade: 10000000000,
+  limit: 256,
+};
+rigData[6] = {
+  price: 300000000000,
+  eth: 0,
+  prod: 500000,
+  upgrade: 100000000000,
+  limit: 256,
+};
+rigData[7] = {
+  price: 0,
+  eth: 5.0,
+  prod: 3000000,
+  upgrade: 12500000000000,
+  limit: 256,
+};
+rigData[8] = {
+  price: 100000000000000,
+  eth: 0,
+  prod: 30000000,
+  upgrade: 50000000000000,
+  limit: 256,
+};
 
 /*
         boostData[0] = BoostData(30, 0.001 ether);
@@ -54,9 +90,9 @@ rigData[8] = {price: 100000000000000,  eth:  0,         prod: 30000000,       up
 */
 boostData = [];
 
-boostData[0] = {bonus: 30, price: 0.05};
-boostData[1] = {bonus: 50, price: 5};
-boostData[2] = {bonus: 100, price: 50};
+boostData[0] = { bonus: 30, price: 0.05 };
+boostData[1] = { bonus: 50, price: 5 };
+boostData[2] = { bonus: 100, price: 50 };
 
 /*
         troopData[0] = TroopData(1,     0,     100000,   0);
@@ -67,13 +103,11 @@ boostData[2] = {bonus: 100, price: 50};
         troopData[5] = TroopData(0,     10000, 0,        0.01 ether);
 */
 
-
 troopData = [];
 
-troopData[0] = {attack: 10,  deffense: 0,    price: 100000,      eth: 0};
-troopData[1] = {attack: 1000,  deffense: 0,  price: 80000000,    eth: 0};
-troopData[2] = {attack: 100000, deffense: 0, price: 0,           eth: 0.5};
-troopData[3] = {attack: 0,    deffense: 15,  price: 100000,      eth: 0};
-troopData[4] = {attack: 0,  deffense: 1500,  price: 80000000,    eth: 0};
-troopData[5] = {attack: 0, deffense: 150000, price: 0,           eth: 0.5};
-
+troopData[0] = { attack: 10, deffense: 0, price: 100000, eth: 0 };
+troopData[1] = { attack: 1000, deffense: 0, price: 80000000, eth: 0 };
+troopData[2] = { attack: 100000, deffense: 0, price: 0, eth: 0.5 };
+troopData[3] = { attack: 0, deffense: 15, price: 100000, eth: 0 };
+troopData[4] = { attack: 0, deffense: 1500, price: 80000000, eth: 0 };
+troopData[5] = { attack: 0, deffense: 150000, price: 0, eth: 0.5 };
